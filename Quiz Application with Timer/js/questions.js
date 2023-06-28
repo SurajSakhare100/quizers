ba2ebb0c121fbe3fab86a34755d6b0c75f46e01a
suraj sakhare <122685473+SurajSakhare100@users.noreply.github.com>
2023-06-28 19:54:33 +0530

    Add files via upload

---
+++ Quiz Application with Timer/js/questions.js
@@ -0,0 +1,114 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+  {
+    numb: 1,
+    question: "Which of the following is not a valid variable name in Python?",
+    answer: "1variable",
+    options: [
+
+      "myVariable",
+      "_variable",
+      "1variable",
+      "variable123"
+
+
+    ]
+  },
+  {
+    numb: 2,
+    question: "What is the purpose of the if statement in Python?",
+    answer: "To make decisions based on conditions",
+    options: [
+      "To define a loop",
+      "To perform type casting",
+      "To handle exceptions",
+      "To make decisions based on conditions"
+    ]
+  },
+  {
+    numb: 3,
+    question: "Which of the following is an example of a mutable data type in Python?",
+    answer: "List",
+    options: [
+      "Tuple",
+      "String",
+      "List",
+      "Set"
+
+    ]
+  },
+  {
+    numb: 4,
+    question: "Which of the following statements is true about Python dictionaries?",
+    answer: "Dictionaries are mutable data types.",
+    options: [
+      "Dictionaries preserve the order of elements.",
+      "Dictionaries can contain duplicate keys.",
+      "Dictionaries are indexed using integers.",
+      "Dictionaries are mutable data types."
+      
+    ]
+  },
+  {
+    numb: 5,
+    question: "What is the purpose of the append() method in Python?",
+    answer: "To add an element to a list",
+    options: [
+      "To add an element to a list",
+      "To remove an element from a list",
+      "To sort a list in ascending order",
+      "To reverse the order of elements in a list"]
+  },
+
+  {
+    numb: 6,
+    question: "Which of the following is used to open a file in Python?",
+    answer: "open()",
+    options: [
+      "read()",
+      "open()",
+      "input()",
+      "close()"
+    ]
+  }, {
+    numb: 7,
+    question: "Which of the following is used to define a function in Python?",
+    answer: "def",
+    options: [
+      "def",
+      "function",
+      "define",
+      "fn"
+    ]
+  }, {
+    numb: 8,
+    question: "What is the purpose of the method in Python?",
+    answer: " To remove leading and trailing whitespaces from a string",
+    options: [
+      " To split a string into a list",
+      " To remove leading and trailing whitespaces from a string",
+      " To concatenate two strings",
+      " To convert a string to uppercase"
+    ]
+  }, {
+    numb: 9,
+    question: "What does the  keyword do in Python?",
+    answer: "It includes a module or library in the current program",
+    options: [
+      "It exports variables to another module",
+      "It defines a new class",
+      "It includes a module or library in the current program",
+      "It converts code into bytecode"
+    ]
+  }, {
+    numb: 10,
+    question: "Which of the following is not a valid data type in Python?",
+    answer: "Loop",
+    options: [
+      "Integer",
+      "String",
+      "Boolean",
+      "Loop"
+    ]
+  },
+];
